ace49bd80256e84e2d509cfc6833164c88ef9b33
Mr.Iffa <90546871+Naufaliffa@users.noreply.github.com>
2023-11-30 00:08:22 +0700

    membuat dataset
    untuk membuat dataset, silahkan uncomment pemanggilan fungsi yang paling bawah. pilih salah satu saja

--- PrepareData.py
+++ PrepareData.py
@@ -1,7 +1,6 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-
 def rekam_wakeword_audio(save_path, n_kali=20):
     input("Untuk merekam audio wake word tekan Enter : ")
     for i in range (n_kali):
@@ -27,6 +26,6 @@ def rekam_audio_background(save_path, n_kali=5):
 #print("Merekam WakeWord Audio: \n")
 #rekam_wakeword_audio("dataaudio/")
 
-print("Merekam WakeWord Audio: \n")
-rekam_audio_background("bgaudio/")
+#print("Merekam WakeWord Audio: \n")
+#rekam_audio_background("bgaudio/")
 
